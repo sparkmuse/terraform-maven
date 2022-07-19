835cb9013c25c7c936e954a688661c09ba24fe92
sparkmuse <alfredo.lopez002@gmail.com>
2022-07-19 20:52:15 +0200

    simplify code

--- test/src/test/java/com/github/sparkmuse/S3Test.java
+++ test/src/test/java/com/github/sparkmuse/S3Test.java
@@ -25,8 +25,6 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 class S3Test {
 
-    private static final int LOCAL_STACK_PORT = 4566;
-
     private static LocalStackContainer container;
     private static Terraform terraform;
 
@@ -34,7 +32,7 @@ class S3Test {
     static void setup(@TempDir Path targetDir) throws Exception {
         container = new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.14.4"))
                 .withServices(LocalStackContainer.Service.values())
-                .withExposedPorts(LOCAL_STACK_PORT);
+                .withExposedPorts(4566);
         container.start();
 
         FileUtils.copyDirectory(Paths.get("").toAbsolutePath().getParent().toFile(), targetDir.toFile());
@@ -44,7 +42,7 @@ class S3Test {
                 .withRootDir(rootDir.toString())
                 .withProperties(Map.of(
                         TerraformApply.Option.tfVars.toString(),
-                        String.format("url=http://%s:%s", container.getHost(), container.getMappedPort(LOCAL_STACK_PORT))
+                        String.format("url=http://%s:%s", container.getHost(), container.getMappedPort(4566))
                 ));
     }
 
@@ -57,7 +55,7 @@ class S3Test {
     @Test
     void createsBucket() throws Exception {
 
-        terraform.init();
+        terraform.getInit().execute(terraform.getProperties());
         terraform.getPlan().execute(terraform.getProperties());
         terraform.getApply().execute(terraform.getProperties());
 
